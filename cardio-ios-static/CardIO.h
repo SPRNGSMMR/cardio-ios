2c941add8a4a742cdf530d5c3d08692dc64e1504
Sylvain Reucherand <sylvain.reucherand@gmail.com>
2016-07-13 18:47:31 +0200

    Public API

--- cardio-ios-static/CardIO.h
+++ cardio-ios-static/CardIO.h
@@ -1,13 +1,9 @@
 //
-//  cardio_ios_static.m
+//  cardio_ios_static.h
 //  cardio-ios-static
 //
 //  Created by Sylvain Reucherand on 13/07/16.
 //  Copyright © 2016 Sylvain Reucherand. All rights reserved.
 //
 
-#import "cardio_ios_static.h"
-
-@implementation cardio_ios_static
-
-@end
+#import "CardView.h"
