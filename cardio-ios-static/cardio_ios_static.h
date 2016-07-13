75bfc23513e4a5084e8ff1a0261c8b032d0534c1
Sylvain Reucherand <sylvain.reucherand@gmail.com>
2016-07-13 18:44:56 +0200

    Split internal logic from demo - Target for static release

---
+++ cardio-ios-static/cardio_ios_static.h
@@ -0,0 +1,13 @@
+//
+//  cardio_ios_static.h
+//  cardio-ios-static
+//
+//  Created by Sylvain Reucherand on 13/07/16.
+//  Copyright © 2016 Sylvain Reucherand. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cardio_ios_static : NSObject
+
+@end
---
+++ cardio-ios-static/cardio_ios_static.m
@@ -0,0 +1,13 @@
+//
+//  cardio_ios_static.m
+//  cardio-ios-static
+//
+//  Created by Sylvain Reucherand on 13/07/16.
+//  Copyright © 2016 Sylvain Reucherand. All rights reserved.
+//
+
+#import "cardio_ios_static.h"
+
+@implementation cardio_ios_static
+
+@end
